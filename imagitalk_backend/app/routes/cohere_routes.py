969a2582ac68404d1641d3d175529980f7f0b129
AryanK1511 <aryankhurana1511@gmail.com>
2024-01-27 16:45:52 -0500

    Created functionality to chat

--- imagitalk_backend/app/routes/cohere_routes.py
+++ imagitalk_backend/app/routes/cohere_routes.py
@@ -3,6 +3,16 @@ import cohere
 import os
 from dotenv import load_dotenv
 from flask import request, jsonify
+from flask import request, jsonify, make_response
+from app import app, db
+import json
+import os
+from urllib.parse import quote_plus, urlencode
+from authlib.integrations.flask_client import OAuth
+from dotenv import find_dotenv, load_dotenv
+from flask import Flask, redirect, render_template, session, url_for
+from app.models.user import User
+from app.models.character import Character
 
 # Configure environment variables
 load_dotenv()
