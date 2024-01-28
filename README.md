eea3cad3145a8f70bb028ab2a498ef207db50b2d
AryanK1511 <aryankhurana1511@gmail.com>
2024-01-28 07:29:43 -0500

    Made final changes

---
+++ README.md
@@ -0,0 +1,16 @@
+# ImagiTalk Web Application
+
+ImagiTalk is an innovative web application that combines nostalgia and cutting-edge artificial intelligence to bring your favorite childhood cartoon characters to life. It's a digital platform where fantasy meets reality, allowing users to interact with AI-driven personas of various beloved characters from their childhood
+
+## Table of Contents
+|Topic|Link|
+|---|---|
+|[App Demo]()||
+|[DevPost]()||
+|[How to use?]()||
+|[Documentation]()||
+
+### Team Members
+[Aryan Khurana]() <br />
+[Arina Kolodeznikova]() <br />
+[Davyd Kuleba]() <br />
---
+++ imagitalk_frontend/components/Navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Navbar as BootstrapNavbar, Nav } from "react-bootstrap";
+import styles from "./Navbar.module.css"; // Import your custom CSS file
+
+const Navbar = () => {
+  return (
+    <BootstrapNavbar
+      className={`${styles.navbarCustom} justify-content-between px-4 py-3`}
+    >
+      <BootstrapNavbar.Brand href="/">
+        <img alt="Logo" src="/assets/images/logo.png" className={styles.logo} />
+      </BootstrapNavbar.Brand>
+      <Nav>
+        <Nav.Link className={`${styles.navLinkCustom}`} href="/">
+          HOME
+        </Nav.Link>
+        {/* <Nav.Link className={`${styles.navLinkCustom}`} href="/api/auth/login">
+          Login
+        </Nav.Link> */}
+      </Nav>
+    </BootstrapNavbar>
+  );
+};
+
+export default Navbar;
